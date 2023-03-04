539b1a41c7c51231186840e25a6e663eb1038056
tlfivem <tldevfivem@gmail.com>
2023-03-03 20:57:23 -0800

    jsjs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
-var music=document.getElementById("music"),isPlaying=!0,button=document.getElementById("toggle");function togglePlay(){isPlaying?music.pause():music.play()}isPlaying&&button.classList.add("rainbow-text"),music.volume=.2,music.onplaying=function(){isPlaying=!0},music.onpause=function(){isPlaying=!1};
+var music=document.getElementById("music").play();
+
+//,isPlaying=!0,button=document.getElementById("toggle");function togglePlay(){isPlaying?music.pause():music.play()}isPlaying&&button.classList.add("rainbow-text"),music.volume=.2,music.onplaying=function(){isPlaying=!0},music.onpause=function(){isPlaying=!1};
